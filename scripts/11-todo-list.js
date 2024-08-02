09bdecd7c0e77e7ef8331ebd2b94fa6be4d4b47c
Begüm Başovalı <begumbasovali@posta.mu.edu.tr>
2024-08-02 11:19:10 +0300

    First version of ToDo List

---
+++ scripts/11-todo-list.js
@@ -0,0 +1,15 @@
+const todoList = [];
+
+function addToDo(){
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  todoList.push(name);
+  console.log(todoList);
+
+}
+
+  
+
+
+ 
